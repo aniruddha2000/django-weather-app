b2a8c6aaaf7144d68391e1d2d3aefe69402c15a0
aniruddha2000 <codewithaniruddha@gmail.com>
2018-12-23 22:26:17 +0530

    move the API part to the post method

--- weatherworld/weather/views.py
+++ weatherworld/weather/views.py
@@ -10,6 +10,32 @@ class WeatherIndex(View):
 
 
     def get(self, request):
+        # cities = City.objects.all()
+        # url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=368a136ce5d41f1d9c904c8201b2d4b5'
+        # weather_data = []
+        # for city in cities:
+        #     city_weather = requests.get(url.format(city)).json()
+        #     weather = {
+        #         'city': city,
+        #         'temperature': city_weather['main']['temp'],
+        #         'description': city_weather['weather'][0]['description'],
+        #         'icon': city_weather['weather'][0]['icon']
+        #     }
+        #
+        #     weather_data.append(weather)
+        form = CityForm
+        context = {
+            'form': form,
+        }
+        return render(request, self.template_name, context)
+
+
+    def post(self, request):
+        form = CityForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
         cities = City.objects.all()
         url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=368a136ce5d41f1d9c904c8201b2d4b5'
         weather_data = []
@@ -28,14 +54,5 @@ class WeatherIndex(View):
             'weather_data': weather_data,
             'form': form
         }
-        return  render(request, self.template_name, context)
-
-
-    def post(self, request):
-        form = CityForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-
-        context = {'form': form}
+        
         return render(request, self.template_name, context)
