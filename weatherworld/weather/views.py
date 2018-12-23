504386d9b84f07947567cbcc6150d64ae45dd8eb
aniruddha2000 <codewithaniruddha@gmail.com>
2018-12-23 22:32:50 +0530

    some pylint stuff

--- weatherworld/weather/views.py
+++ weatherworld/weather/views.py
@@ -1,28 +1,17 @@
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import View
+import requests
 from .models import City
 from .forms import CityForm
-import requests
-from django.shortcuts import render
+
 
 class WeatherIndex(View):
 
     template_name = 'weather/city_create.html'
-
+    queryset = get_object_or_404(City)
 
     def get(self, request):
-        # cities = City.objects.all()
-        # url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=368a136ce5d41f1d9c904c8201b2d4b5'
-        # weather_data = []
-        # for city in cities:
-        #     city_weather = requests.get(url.format(city)).json()
-        #     weather = {
-        #         'city': city,
-        #         'temperature': city_weather['main']['temp'],
-        #         'description': city_weather['weather'][0]['description'],
-        #         'icon': city_weather['weather'][0]['icon']
-        #     }
-        #
-        #     weather_data.append(weather)
+
         form = CityForm
         context = {
             'form': form,
@@ -36,8 +25,9 @@ class WeatherIndex(View):
         if form.is_valid():
             form.save()
 
-        cities = City.objects.all()
-        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=368a136ce5d41f1d9c904c8201b2d4b5'
+        cities = self.queryset
+        api_key = '368a136ce5d41f1d9c904c8201b2d4b5'
+        url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=' + api_key
         weather_data = []
         for city in cities:
             city_weather = requests.get(url.format(city)).json()
@@ -54,5 +44,5 @@ class WeatherIndex(View):
             'weather_data': weather_data,
             'form': form
         }
-        
+
         return render(request, self.template_name, context)
